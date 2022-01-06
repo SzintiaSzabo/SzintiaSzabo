92529ab7dba39cd6f73793c860f459f622e44e4d
SzintiaSzabo <91495588+SzintiaSzabo@users.noreply.github.com>
2022-01-06 09:50:40 +0200

    Add files via upload

---
+++ hazi_8/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int joc(int n){
+int k=0;
+for(int i=2; i<=n; i++){
+	if(n%i==0){
+		k++;
+	}
+}
+return k;
+}
+
+int main()
+{
+		int n;
+		cout << "n=";
+		cin >> n;
+		cout << joc(n);
+    return 0;
+}

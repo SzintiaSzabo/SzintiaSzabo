0a386d0e7fd84ce06e2bc214d86cccb80fe769a3
SzintiaSzabo <91495588+SzintiaSzabo@users.noreply.github.com>
2023-02-16 08:39:22 +0200

    Add files via upload

---
+++ poterettsegi2021_3_3/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int tukor(int a){
+int ujszam=0,szj;
+if(a<10){
+    return a*10;
+}
+while(a>0){
+    szj=a%10;
+    a/=10;
+    ujszam=ujszam*10+szj;
+    }
+return ujszam;
+}
+
+int main()
+{
+    ifstream in("numere.in");
+    int na,nb,a[100]={0},s=0,x;
+    in>>na;
+    in>>nb;
+    for(int i=0;i<na;i++){
+      in>>x;
+      a[x%100] ++;
+    }
+
+    for(int i=0;i<nb;i++){
+      in>>x;
+      x=x%100;
+      if(a[x]>0){
+        s+=a[x];
+
+      }
+      int y=tukor(x);
+      if(a[y]>0){
+            if(y&10!=y/10){
+        s+=a[y];
+      }
+      }
+
+    }
+
+    cout<<s;
+    return 0;
+}
